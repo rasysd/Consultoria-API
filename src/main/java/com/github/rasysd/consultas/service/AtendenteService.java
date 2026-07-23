0470f384c086bfe23f48c238fd562d749de6956f
Rayson_Jonatha_DEV <108767094+rasysd@users.noreply.github.com>
2026-07-23 18:16:51 -0300

    Feat: adicionado criptografia e dependcias no pom.xml

--- src/main/java/com/github/rasysd/consultas/service/AtendenteService.java
+++ src/main/java/com/github/rasysd/consultas/service/AtendenteService.java
@@ -2,17 +2,22 @@ package com.github.rasysd.consultas.service;
 
 import com.github.rasysd.consultas.model.Atendente;
 import com.github.rasysd.consultas.repository.AtendenteRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AtendenteService {
     private final AtendenteRepository atendenteRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AtendenteService(AtendenteRepository atendenteRepository) {
+    public AtendenteService(AtendenteRepository atendenteRepository, PasswordEncoder passwordEncoder) {
         this.atendenteRepository = atendenteRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Atendente cadastrarAtendente(Atendente atendente) {
+        String senhaSegura = this.passwordEncoder.encode(atendente.getSenha());
+        atendente.setSenha(senhaSegura);
         return this.atendenteRepository.save(atendente);
     }
 }
--- src/main/java/com/github/rasysd/consultas/service/ClienteService.java
+++ src/main/java/com/github/rasysd/consultas/service/ClienteService.java
@@ -2,17 +2,22 @@ package com.github.rasysd.consultas.service;
 
 import com.github.rasysd.consultas.model.Cliente;
 import com.github.rasysd.consultas.repository.ClienteRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
     private final ClienteRepository clienteRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public ClienteService(ClienteRepository clienteRepository) {
+    public ClienteService(ClienteRepository clienteRepository, PasswordEncoder passwordEncoder) {
         this.clienteRepository = clienteRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Cliente cadastrarCliente(Cliente cliente){
+        String senhaSegura = this.passwordEncoder.encode(cliente.getSenha());
+        cliente.setSenha(senhaSegura);
         return this.clienteRepository.save(cliente);
     }
 
